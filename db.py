04f9752672d18649e7296b7ad753d555ca0104d6
NotKatsu <katsudiscord@outlook.com>
2023-03-05 16:40:05 +0000

    Added table setup

--- db.py
+++ db.py
@@ -1,19 +1,28 @@
 import sqlite3 
 
-def createDatabase(databaseName: str) -> object: 
+def createConnection(databaseName: str) -> object: 
     connection = sqlite3.connect(databaseName)
     return connection
 
 def tableSetup(connectionObject: object) -> bool: 
-    print(connectionObject)
+    cursor = connectionObject.cursor() 
+    cursor.execute("""CREATE TABLE IF NOT EXISTS countingChannels (
+                        guild BIGINT,
+                        channel BIGINT
+    )""")
 
 def databaseSetup(databaseName: str) -> bool: 
     try:
-        connection = createDatabase(databaseName)
+        connection = createConnection(databaseName)
         if tableSetup(connection) == False: 
             return False 
         else:
             return True
     except:
         return False
+    
+def addChannel(channel: int) -> bool: 
+    print(channel)
+    
+
     
