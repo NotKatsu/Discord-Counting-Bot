45ed18b5e4824b9d86faf028831eca6be233a2b9
NotKatsu <katsudiscord@outlook.com>
2023-03-05 16:40:35 +0000

    Added configCommands.py

--- cogs/configCommands.py
+++ cogs/configCommands.py
@@ -1,12 +1,21 @@
-from discord.commands import SlashCommandGroup
+import discord
+
+from db import addChannel
 from discord.ext import commands
+from discord.commands import SlashCommandGroup
+
 
 class configCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+
     config = SlashCommandGroup("config", "Commands that allow you to control the bots configuration.")
 
+
+    @config.command() 
+    async def channel(self, ctx, channel: discord.TextChannel): 
+        addChannel(channel.id)
 
 
 def setup(bot):
