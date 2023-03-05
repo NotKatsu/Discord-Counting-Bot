c1b54680712f475f0ac7145cd9dd9e07a267cf82
NotKatsu <katsudiscord@outlook.com>
2023-03-05 17:19:18 +0000

    Added missing permissions embed

--- cogs/messageEvents.py
+++ cogs/messageEvents.py
@@ -1,9 +1,26 @@
+import discord
+
 from discord.commands import SlashCommandGroup
 from discord.ext import commands
 
 class messageEvents(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.Cog.listener() 
+    async def on_message(self, message):
+        print(message.content)
+        
+
+    @commands.Cog.listener()
+    async def on_application_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            missing_permissions = error.missing_permissions
+            missingperms = discord.Embed(title=f" Missing Permissions",
+                                         colour=discord.Colour(0xFAA61A),
+                                         description=f"Sorry but you need the `{missing_permissions[0]}` permission to use that command.")
+
+            await ctx.respond(embed=missingperms, ephemeral=True)
 
 
 def setup(bot):
