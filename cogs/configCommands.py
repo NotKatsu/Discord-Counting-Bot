4c8d8a3fae02bd4d68d0ecc461a9aed93ac7bcfe
NotKatsu <katsudiscord@outlook.com>
2023-03-05 17:15:02 +0000

    Config Channel command complete

--- cogs/configCommands.py
+++ cogs/configCommands.py
@@ -15,7 +15,21 @@ class configCommands(commands.Cog):
 
     @config.command() 
     async def channel(self, ctx, channel: discord.TextChannel): 
-        addChannel(channel.id)
+        """Set the Counting Channel for members to count in."""
+        if addChannel(channel) is True: 
+            setChannel = discord.Embed(title="Set Counting Channel.",
+                                    colour=discord.Colour(0xFAA61A),
+                                    description=f"Set Counting channel to {channel.mention} `({channel.id})`")
+
+            setChannel.set_footer(text=f"Channel Set • Counting Channel {channel.id}")
+            await ctx.respond(embed=setChannel)
+        else:
+            Error = discord.Embed(title="Failed To Set Counting Channel.",
+                                    colour=discord.Colour(0xFAA61A),
+                                    description=f"I couldn't set the counting channel for this server.")
+
+            Error.set_footer(text=f"Error • Could't Set Channel")
+            await ctx.respond(embed=Error)
 
 
 def setup(bot):
