cc722ce165bf9b16e27f1b55399936828e6466be
NotKatsu <katsudiscord@outlook.com>
2023-03-05 19:29:49 +0000

    Count is now updated

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 import sqlite3 
 
+from typing import Union
+
 def createConnection(databaseName: str) -> object: 
     connection = sqlite3.connect(databaseName)
     return connection
@@ -9,7 +11,8 @@ def tableSetup(connectionObject: object) -> bool:
         cursor = connectionObject.cursor() 
         cursor.execute("""CREATE TABLE IF NOT EXISTS countingChannels (
                             guild BIGINT,
-                            channel BIGINT
+                            channel BIGINT,
+                            count BIGINT
         )""")
         return True
     except:
@@ -33,15 +36,62 @@ def addChannel(channel: object) -> bool:
         response = cursor.execute("SELECT channel FROM countingChannels WHERE guild = ?", (channel.guild.id,))
 
         if response.fetchone() is None: 
-            cursor.execute("INSERT INTO countingChannels (guild, channel) VALUES (?, ?)", (channel.guild.id, channel.id))
+            cursor.execute("INSERT INTO countingChannels (guild, channel, count) VALUES (?, ?, ?)", (channel.guild.id, channel.id, 0))
             connection.commit()
         else:
-            cursor.execute("UPDATE countingChannels SET channel = ? WHERE guild = ?", (channel.id, channel.guild.id))
+            cursor.execute("DELETE FROM countingChannels WHERE guild = ?", (channel.guild.id,))
+            cursor.execute("INSERT INTO countingChannels (guild, channel, count) VALUES (?, ?, ?)", (channel.guild.id, channel.id, 0))
             connection.commit()
             
         return True
-    except: 
+    except Exception as e:
+        print(e) 
         return False
     
-def addToCount(message: object) -> str:
-    print(message.content)
+def getChannel(message: object) -> Union[int, str]: 
+    try:
+        connection = createConnection("database.db")
+        cursor = connection.cursor() 
+
+        cursor.execute("SELECT channel, count FROM countingChannels WHERE guild = ?", (message.guild.id,))
+
+        return cursor.fetchone()[0]
+    except Exception as e: 
+        print(e);return "error"
+
+def getCount(message: object) -> Union[int, str]: 
+    try:
+        connection = createConnection("database.db")
+        cursor = connection.cursor() 
+
+        cursor.execute("SELECT channel, count FROM countingChannels WHERE guild = ?", (message.guild.id,))
+
+        return cursor.fetchone()[1]
+    except Exception as e: 
+        print(e);return "error"
+
+def updateCount(message: object, count: int) -> Union[int, str]: 
+    connection = createConnection("database.db")
+    cursor = connection.cursor() 
+
+    cursor.execute("UPDATE countingChannels SET count = ? WHERE guild = ?", (count, message.guild.id))
+    connection.commit()
+
+    
+def addToCount(message: object) -> int:
+    if message.content.isnumeric() is True: 
+        currentCount = getCount(message)
+        countnewValue = currentCount + 1
+        countingChannel = getChannel(message)
+
+        if currentCount == "error" or countingChannel == "error":
+            return
+        
+        elif countingChannel == message.channel.id: 
+            if int(countnewValue) == int(message.content): 
+                updateCount(message, countnewValue)
+            else:
+                return 5
+    else: 
+        return 0
+    
