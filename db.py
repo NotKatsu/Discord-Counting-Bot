f01c2c0fd4a39fd6afe0d4d37605512645901f4e
NotKatsu <katsudiscord@outlook.com>
2023-03-05 16:43:21 +0000

    Fixed bug were bot doesn't start

--- db.py
+++ db.py
@@ -5,16 +5,24 @@ def createConnection(databaseName: str) -> object:
     return connection
 
 def tableSetup(connectionObject: object) -> bool: 
-    cursor = connectionObject.cursor() 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS countingChannels (
-                        guild BIGINT,
-                        channel BIGINT
-    )""")
+    print("recieved table creation request")
+    try:
+        cursor = connectionObject.cursor() 
+        cursor.execute("""CREATE TABLE IF NOT EXISTS countingChannels (
+                            guild BIGINT,
+                            channel BIGINT
+        )""")
+        return True
+    except:
+        return False
 
 def databaseSetup(databaseName: str) -> bool: 
+    print("recieved request")
     try:
         connection = createConnection(databaseName)
+        print("created connection")
         if tableSetup(connection) == False: 
+            print("table setup complete")
             return False 
         else:
             return True
